323478507bf3a27e46323d1e39c4ed86b9c69465
Paul Tarvydas <guitarvydas@gmail.com>
2022-01-30 15:17:55 -0500

    kind recognizes gray and red text

--- kind.pl
+++ kind.pl
@@ -17,12 +17,24 @@ kind_format(Out, [[H1,H2]|T], S):-
     kind_format(Out,T,S2),
     string_concat(S1,S2,S).
 
+gray(Node):-diagram_fact(fillColor,Node,"#f5f5f5").
+red(Node):-diagram_fact(fillColor,Node,"#f5f5f5"), \+ gray(Node).
+
+kind(Node,text):-gray(Node).
 kind(Node,sequence):-
-    diagram_fact(value,Node,"#sequence").
+    diagram_fact(value,Node,"#sequence"),
+    \+ gray(Node).
 kind(Node,fallback):-
-    diagram_fact(value,Node,"#fallback").
-kind(Node,action):-
+    diagram_fact(value,Node,"#fallback"),
+    \+ gray(Node).
+kind(Node,syncaction):-
     diagram_fact(value,Node,K),
     K \= "#sequence",
-    K \= "#fallback".
+    K \= "#fallback",
+    red(Node).
+kind(Node,asyncaction):-
+    diagram_fact(value,Node,K),
+    K \= "#sequence",
+    K \= "#fallback",
+    \+ gray(Node).
     
