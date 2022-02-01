2cbac71628151fbdc760477c761d1acd48c94235
Paul Tarvydas <guitarvydas@gmail.com>
2022-02-01 08:51:21 -0500

    rename to tokenize

--- parsecode.bash
+++ parsecode.bash
@@ -2,4 +2,4 @@
 prep=../prep/prep
 ./emitcode.pl >out.txt
 ./emitcpp.py out.txt >out2.txt
-$prep '.' '$' code.ohm code.glue --stop=1 <out2.txt
+$prep '.' '$' tokenize.ohm tokenize.glue --stop=1 <out2.txt
