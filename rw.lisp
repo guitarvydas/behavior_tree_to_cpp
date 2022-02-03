6c60485206234602e8545b14d44c6a1e4c014a59
Paul Tarvydas <guitarvydas@gmail.com>
2022-02-03 06:58:27 -0500

    alpha interpret script rw.lisp

--- rw.lisp
+++ rw.lisp
@@ -1,4 +1,5 @@
 (defparameter *component-as-json* "/Users/tarvydas/projects/t2py/component.json")
+(defparameter *script* "/Users/tarvydas/projects/t2py/rewrite.lisp")
 
 ;; from https://riptutorial.com/common-lisp/example/19473/reading-and-writing-entire-files-to-and-from-strings
 (defun read-file (infile)
@@ -10,4 +11,36 @@
 
 (defun run ()
   (with-open-file (inf *component-as-json* :direction :input)
-    (read-file inf)))
+    (let ((s (read-file inf)))
+      (let ((strm (make-string-input-stream s)))
+        (let ((json-input (cl-json::decode-json strm)))
+          (with-open-file (fscript *script* :direction :input)
+            (let ((script (read fscript)))
+              (interpret-script script json-input))))))))
+
+(defun interpret-script (script object)
+  (when script 
+    (let ((command (first script))
+          (arg (second script)))
+      (cond ((eq 'program command) (iterate-json-array object #'(lambda (item) (interpret-script arg item)))) ;; cdr down complete object
+            ((eq 'foreach command) (let ((field (second script))                                              ;; cdr down field of object
+                                         (subscript (third script)))
+                                     (iterate-json-array (slot-value object field) #'(lambda (item) (interpret-script subscript item)))))
+            ((eq 'textblock command) (interpret-textblock arg object))
+            (t (error "illegal command"))))))
+
+(defun interpret-textblock (script object)
+  (when script
+    (let ((command (first script))
+          (arg (second script)))
+      (cond
+       ((eq 'text command) (output arg))
+       ((eq 'fieldfref command) (output (slot-value object arg)))
+       ((eq 'tosref command) (output object))
+       (t (error "illegal textblock command"))))))
+
+(defun output (x)
+  (format *standard-output* "~a" x))
+
+(defun iterate-json-array (array-object func)
+  (mapc func array-object))
