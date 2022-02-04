626e0f7bdaefb57d79fef8dc3d1923f428034b91
Paul Tarvydas <guitarvydas@gmail.com>
2022-02-03 19:30:31 -0500

    working on join.py writing fd 5

--- pyrun.bash
+++ pyrun.bash
@@ -2,8 +2,6 @@
 set -x
 trap catch ERR
 
-set -x
-
 duct3=duct3_$RANDOM
 duct4=duct4_$RANDOM
 duct5=duct5_$RANDOM
@@ -13,14 +11,14 @@ catch(){
     rm -f $duct3 $duct4 $duct5
     }
 
-./join.py 3<$duct3 &
+./join.py 3<$duct3 4<$duct4 5>$duct5 &
 #./join.py 3<$duct3 4<$duct4 5>$duct5 &
 pid=$!
-echo hello >$duct3 &
-# echo world >$duct4 &
+echo -n 'hello ' >$duct3 &
+echo -n 'world ' >$duct4 &
+echo <$duct5 &
 wait $pid
-# echo <$duct5
-rm -f $duct3
-rm -f $duct4
-rm -f $duct5
+# rm -f $duct3
+# rm -f $duct4
+# rm -f $duct5
 
